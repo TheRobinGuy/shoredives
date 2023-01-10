429bbd7f4282aead6a7ab9f13b0d7e4993cd34c6
TheRobinGuy <gilmartin.glenn@gmail.com>
2023-01-10 16:07:23 +0000

    Changing style on body

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import ImageContainer from './components/ImageContainer.js'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className='bodyWrapper'>
         <ImageContainer></ImageContainer>
-      </header>
+      </div>
     </div>
   );
 }
