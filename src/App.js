193e4d4dddf169def2ba88833fc52eaffe9ade95
TheRobinGuy <gilmartin.glenn@gmail.com>
2023-01-10 16:31:30 +0000

    Adding skeleton navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import ImageContainer from './components/ImageContainer.js'
+import NavBar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <div className='bodyWrapper'>
+        <NavBar></NavBar>
         <ImageContainer></ImageContainer>
       </div>
     </div>
